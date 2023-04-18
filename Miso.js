7a4d32c03919d45fe23f2186827b518daa5d99ca
stephfff528 <70963311+stephfff528@users.noreply.github.com>
2023-04-18 15:16:11 -0400

    .

--- Miso.js
+++ Miso.js
@@ -23,7 +23,7 @@
       circle.style.setProperty("--xB", getRandomNumber(-600, 600) + "px");
       circle.style.setProperty("--yB", getRandomNumber(-600, 600) + "px");
 
-      console.log(circle);
+      // console.log(circle);
     }
 
     function getRandomNumber(low, high) {
@@ -32,7 +32,57 @@
     }
 
 
+  var count = 0 
+  // var body = document.getElementById("body")
+  var large = document.getElementById("large")
+  var medium = document.getElementById("medium")
+  var small = document.getElementById("small")
+  var score = document.getElementById("score")
 
+
+  document.body.onclick = function (){
+    count++;
+    score.innerHTML = count;
+    console.log("body clicked!!!");
+  }
+
+
+  var large_circles = document.getElementsByClassName("large");
+
+  var large_click = function() {
+      count = count+1;
+      score.innerHTML = count;
+      console.log("large circle clicked!!!");
+  };
+
+  for (var i = 0; i < large_circles.length; i++) {
+      large_circles[i].addEventListener('click', large_click, false);
+  }
+
+   var medium_circles = document.getElementsByClassName("medium");
+
+   var medium_click = function() {
+      count = count+2;
+      score.innerHTML = count;
+      console.log("medium circle clicked!!!");
+  };
+
+  for (var i = 0; i < medium_circles.length; i++) {
+      medium_circles[i].addEventListener('click', medium_click, false);
+  }
+
+  var small_circles = document.getElementsByClassName("small");
+
+   var small_click = function() {
+      count = count+3;
+      score.innerHTML = count;
+      console.log("small circle clicked!!!");
+  };
+
+  for (var i = 0; i < small_circles.length; i++) {
+      small_circles[i].addEventListener('click', small_click, false);
+  }
+  
 
 
 
