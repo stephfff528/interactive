3ad7bc95c81623817e5b58f0fd6996b12f42d5cb
stephfff528 <70963311+stephfff528@users.noreply.github.com>
2023-04-20 00:58:19 -0400

    .

--- Miso.js
+++ Miso.js
@@ -1,130 +1,141 @@
+let container = document.querySelector("body");
+container.addEventListener("animationend", changePosition, true);
 
- let container = document.querySelector(".circle");
-    container.addEventListener("animationend", changePosition, true);
-
-
-
-    function changePosition(event) {
-      let circle = event.target;
-
-      circle.style.animationName = "none";
+function changePosition(event) {
+  let circle = event.target;
+  circle.style.animationName = "none";
       
-      requestAnimationFrame(() => {
-        circle.style.animationName = "";
+  requestAnimationFrame(() => {
+    circle.style.animationName = "";
       });
 
-      let circleStyle = getComputedStyle(circle);
-      let finalX = circleStyle.getPropertyValue("--xB");
-      let finalY = circleStyle.getPropertyValue("--yB");
+  let circleStyle = getComputedStyle(circle);
+  let finalX = circleStyle.getPropertyValue("--xB");
+  let finalY = circleStyle.getPropertyValue("--yB");
 
-      circle.style.setProperty("--xA", finalX);
-      circle.style.setProperty("--yA", finalY);
+  circle.style.setProperty("--xA", finalX);
+  circle.style.setProperty("--yA", finalY);
 
-      circle.style.setProperty("--xB", getRandomNumber(-600, 600) + "px");
-      circle.style.setProperty("--yB", getRandomNumber(-600, 600) + "px");
+  circle.style.setProperty("--xB", getRandomNumber(-600, 600) + "px");
+  circle.style.setProperty("--yB", getRandomNumber(-600, 600) + "px");
 
-      // console.log(circle);
-    }
-
-    function getRandomNumber(low, high) {
+  function getRandomNumber(low, high) {
       let r = Math.floor(Math.random() * (high - low + 1)) + low;
       return r;
-    }
-
-
-  var count = 0 
-  // var body = document.getElementById("body")
-  var large = document.getElementById("large")
-  var medium = document.getElementById("medium")
-  var small = document.getElementById("small")
-  var score = document.getElementById("score")
-
-
-  var bodypts = document.getElementById("bodypts");
-
-  function hide_pts() = {
-  bodypts.style.display = "none";
+  }
+  // console.log(circle);
 }
 
+var count = 0 
+var score = document.getElementById("score")
 
 
-  document.body.onclick = function (){
-    count++;
-    score.innerHTML = count;
+document.body.onclick = function (){
+  count++;
+  score.innerHTML = count;
+  console.log("body clicked!!!");
+}
 
-
-
-    // document.getElementById("bodypts");
-    // bodypts.style.display = "none";
-    bodypts.style.display = "block";
-    setTimeout(hide_pts, 1000);
-    console.log("body clicked!!!");
-  }
-
-
-  var large_circles = document.getElementsByClassName("large");
+var large_circles = document.getElementsByClassName("large");
+var large_click = function() {
+  count = count+1;
+  score.innerHTML = count;
+  console.log("large circle clicked!!!");
 
   large_array = [
     'bird.mp4',
-    'mouse.mp4',
-    'koi.mp4',
     'flies.mp4',
+    'koi.mp4',
+    'mouse.mp4',
   ]
 
-  var large_click = function() {
-
-      count = count+1;
-      score.innerHTML = count;
-
-      random_index = Math.floor(Math.random() * large_array.length);
-      selected_large = large_array[random_index];
-      document.getElementById("largevid").src = `./largevid/${selected_large}`;
-
-      
-      var largevid = document.getElementById("largevid")
-
-      largevid.style.display = "block";
-      largevid.play();
-      vid.muted = false;
-      largevid.addEventListener('ended', function() {
-        bird.style.display = "none";
-
+  random_index = Math.floor(Math.random() * large_array.length);
+  selected_large = large_array[random_index];
+  document.getElementById("largevid").src = `./largevid/${selected_large}`;
+  largevid.style.display = "block";
+  largevid.play();
+  largevid.muted = false;
+  largevid.addEventListener('ended', function() {
+    largevid.style.display = "none";
   })
-      console.log("large circle clicked!!!");
-  };
-  for (var i = 0; i < large_circles.length; i++) {
-      large_circles[i].addEventListener('click', large_click, false);
-  }
+};
+
+for (var i = 0; i < large_circles.length; i++) {
+  large_circles[i].addEventListener('click', large_click, false);
+}
+
+
+var medium_circles = document.getElementsByClassName("medium");
+var medium_click = function() {
+  count = count+2;
+  score.innerHTML = count;
+  console.log("medium circle clicked!!!");
+
+  medium_array = [
+    '1.mp4',
+    '2.mp4',
+    '3.mp4',
+    '4.mp4',
+    '5.mp4',
+    '6.mp4',
+    '7.mp4',
+    '8.mp4',
+    '9.mp4',
+    '10.mp4',
+    '11.mp4',
+    '12.mp4',
+  ]
+
+  random_index = Math.floor(Math.random() * medium_array.length);
+  selected_medium = medium_array[random_index];
+  document.getElementById("mediumvid").src = `./mediumvid/${selected_medium}`;
+  mediumvid.style.display = "block";
+  mediumvid.play();
+  mediumvid.muted = false;
+  mediumvid.addEventListener('ended', function() {
+    mediumvid.style.display = "none";
+  })
 
 
 
+};
 
+for (var i = 0; i < medium_circles.length; i++) {
+  medium_circles[i].addEventListener('click', medium_click, false);
+}
 
+var small_circles = document.getElementsByClassName("small");
+var small_click = function() {
+  count = count+3;
+  score.innerHTML = count;
+  console.log("small circle clicked!!!");
 
+  small_array = [
+    'a.mp4',
+    'b.mp4',
+    'c.mp4',
+    'd.mp4',
+    'e.mp4',
+    'f.mp4',
+    'g.mp4',
+    'h.mp4',
+  ]
 
-   var medium_circles = document.getElementsByClassName("medium");
+  random_index = Math.floor(Math.random() * small_array.length);
+  selected_small = small_array[random_index];
+  document.getElementById("smallvid").src = `./smallvid/${selected_small}`;
+  smallvid.style.display = "block";
+  smallvid.play();
+  smallvid.muted = false;
+  smallvid.addEventListener('ended', function() {
+    smallvid.style.display = "none";
+  })
 
-   var medium_click = function() {
-      count = count+2;
-      score.innerHTML = count;
-      console.log("medium circle clicked!!!");
-  };
+};
 
-  for (var i = 0; i < medium_circles.length; i++) {
-      medium_circles[i].addEventListener('click', medium_click, false);
-  }
-
-  var small_circles = document.getElementsByClassName("small");
-
-   var small_click = function() {
-      count = count+3;
-      score.innerHTML = count;
-      console.log("small circle clicked!!!");
-  };
-
-  for (var i = 0; i < small_circles.length; i++) {
-      small_circles[i].addEventListener('click', small_click, false);
-  }
+for (var i = 0; i < small_circles.length; i++) {
+  small_circles[i].addEventListener('click', small_click, false);
+}
   
 
 
